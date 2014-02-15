a1222f1f9487660e83b6b57450cb337342b83669
kingcody <me@codymize.com>
2014-02-14 23:38:31 -0500

    adding archlinux support to params and using puppetmasterpkg and puppetagentpkg

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,18 +5,35 @@ class puppet::params {
   $logdir = '/var/log/puppet'
 
   case $::operatingsystem {
-    'Archlinux',
+    'Archlinux': {
+      $puppetmasterpkg = 'puppet'
+      $puppetagentpkg = 'puppet'
+      $sysconfig = true
+      $rundir = '/var/run/puppet'
+    }
     'Fedora',
     'RedHat',
     'CentOS': {
+      $puppetmasterpkg = 'puppet-server'
+      $puppetagentpkg = 'puppet'
       $sysconfig = true
       $rundir = '/var/run/puppet'
     }
     'Gentoo': {
+      $puppetmasterpkg = 'puppet'
+      $puppetagentpkg = 'puppet'
+      $sysconfig = false
+      $rundir = '/run/puppet'
+    }
+    'Debian': {
+      $puppetmasterpkg = 'puppetmaster'
+      $puppetagentpkg = 'puppet'
       $sysconfig = false
       $rundir = '/run/puppet'
     }
     default: {
+      $puppetmasterpkg = 'puppet-server'
+      $puppetagentpkg = 'puppet'
       $sysconfig = false
       $rundir = '/var/run/puppet'
     }
